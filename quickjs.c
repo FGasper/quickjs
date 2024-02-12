8c9611cd0f04b20f1f0293de48c2df5526665172
Felipe Gasper <felipe@felipegasper.com>
2024-02-12 00:12:17 -0500

    no assert

--- quickjs.c
+++ quickjs.c
@@ -43171,10 +43171,11 @@ static const JSCFunctionListEntry js_math_obj[] = {
    between UTC time and local time 'd' in minutes */
 static int getTimezoneOffset(int64_t time)
 {
-    assert(time >= 0 /* time must be positive */);
     time_t ti;
     int res;
+
     fprintf(stderr, "%s(%" PRId64 ")\n", __func__, time);
+    //assert(time >= 0 /* time must be positive */);
 
     time /= 1000; /* convert to seconds */
     if (sizeof(time_t) == 4) {
